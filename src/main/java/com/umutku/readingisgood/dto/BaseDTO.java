ff6feaa16e84c677e69f618d90b774dc9ec15082
umutku <umututku.tanis@gmail.com>
2022-02-09 21:43:54 +0300

    unit test added to Book domain

--- src/main/java/com/umutku/readingisgood/dto/BaseDTO.java
+++ src/main/java/com/umutku/readingisgood/dto/BaseDTO.java
@@ -1,7 +1,10 @@
 package com.umutku.readingisgood.dto;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
 public class BaseDTO {
     private UUID id;
 }
---
+++ src/test/java/com/umutku/readingisgood/domain/BookTest.java
@@ -0,0 +1,42 @@
+package com.umutku.readingisgood.domain;
+
+import com.umutku.readingisgood.dto.BookDTO;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BookTest {
+
+    @Test
+    void fromDTO() {
+        BookDTO dto = new BookDTO("title", "author", 11);
+        Book book = Book.fromDTO(dto);
+
+        assertEquals(dto.getAuthor(), book.getAuthor());
+        assertEquals(dto.getTitle(), book.getTitle());
+        assertEquals(dto.getStock(), book.getStock());
+    }
+
+    @Test
+    void decreaseStock() {
+        Book book = new Book("title", "author", 11);
+        book.decreaseStock(5);
+        assertEquals(6, book.getStock());
+    }
+
+    @Test
+    void invalidDecreaseStock() {
+        Book book = new Book("title", "author", 11);
+        assertThrows(IllegalArgumentException.class, () -> book.decreaseStock(15));
+    }
+
+    @Test
+    void updateStock() {
+        Book book = new Book("title", "author", 11);
+        int newStock = 111;
+        book.updateStock(newStock);
+        assertEquals(newStock, book.getStock());
+
+    }
+}
